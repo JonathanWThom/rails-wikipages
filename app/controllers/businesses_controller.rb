398d7b0038ffd524959fd410c9f083875b3dfe62
Chris Clifford and Jonathan Thom <chris+jonathan@>
2017-02-06 13:46:44 -0800

    Add businesses to cities

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,6 +1,7 @@
 class BusinessesController < ApplicationController
   def new
     @type = Type.find(params[:type_id])
+    @cities = City.all
     @business = @type.businesses.new
   end
 
@@ -39,6 +40,6 @@ class BusinessesController < ApplicationController
 
   private
   def business_params
-    params.require(:business).permit(:name, :address, :phone_number, :website, :hours)
+    params.require(:business).permit(:name, :address, :phone_number, :website, :hours, :city_id)
   end
 end
