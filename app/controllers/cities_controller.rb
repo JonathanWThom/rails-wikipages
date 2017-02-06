735296f1908b6cc21aa2d9a112257c2ecd62641b
Chris Clifford and Jonathan Thom <chris+jonathan@>
2017-02-06 13:13:30 -0800

    add citie crud functionality

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -4,8 +4,47 @@ class CitiesController < ApplicationController
     render :index
   end
 
+  def new
+    @city = City.new
+  end
+
+  def create
+    @city = City.new(city_params)
+    if @city.save
+      redirect_to cities_path
+    else
+      render :new
+    end
+  end
+
   def show
     @city = City.find(params[:id])
     render :show
+  end
+
+  def edit
+    @city = City.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @city = City.find(params[:id])
+    if @city.update(city_params)
+      redirect_to cities_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @city = City.find(params[:id])
+    @city.destroy
+    redirect_to cities_path
+  end
+
+
+private
+  def city_params
+    params.require(:city).permit(:name)
   end
 end
